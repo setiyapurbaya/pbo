065970777bb6b33ba22e4428a551b9f918cef96f
imam rahman <Windows@Windows-PC>
2016-03-11 11:11:45 +0700

    Merge origin/master

---
+++ pbo/src/pbo/Dosen.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo;
+
+/**
+ *
+ * @author Windows
+ */
+//enkapsulasi class DOSEN//
+public class DOSEN {
+    private String nama;
+    private int nip;
+    private int StatusPembimbing;
+    private KelompokTA topikTA[] t=new KelompokTA[10]; //Array KelompokTA terenkapsulasi private berukuran 10//
+    private int jumlahKelompokTA=0;
+   
+    //method CreateKelompokTA, menginstansiasi kelompokTA baru sesuai input//
+    public void CreateKelompokTA(String topik) {
+        if(jumlahKelompokTA<10) {
+            t[jumlahKelompokTA]=new KelompokTA(topik);
+            jumlahKelompokTA++;
+        }
+    }
+   
+    //constructor untuk mengeset Status Pembimbing//
+    public DOSEN(int statusPembimbing) {
+        setStatusPembimbing(StatusPembimbing);
+    }
+    
+    //method setStatusPembimbing & getStatusPembimbing//
+    public void setStatusPembimbing(int StatusPembimbing) {
+        this.StatusPembimbing = StatusPembimbing;
+    }
+    
+    public int getStatusPembimbing() {
+        return StatusPembimbing;
+    } 
+       
+}
